8a6ca72a6f387e87b2e3a32d2a8f7265909ae07f
kelfimov85-stack <k.elfimov85@gmail.com>
2026-04-23 14:56:13 +0500

    Создал листы для моделей, конструктор и заполнение листов черех LoadData() в PageOne

--- Coursework/ViewModels/PageOne.cs
+++ Coursework/ViewModels/PageOne.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Coursework.Data;
+using Coursework.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +13,39 @@ namespace Coursework.ViewModels
 {
     public partial class PageOne : ViewModelBase
     {
+        private readonly Context db = new();
+
+        //создание спмсков
+        private List<Products> _allProducts = new();
+        private ObservableCollection<Products> Products { get; set; } = new();
+        private ObservableCollection<Categories> Categories { get; set; } = new();
+
+        //[ObservableProperty]
+
+        //конструктор
+        public PageOne() 
+        {
+            LoadData();
+        }
+
+        private async void LoadData()
+        {
+            //очищаем списки
+            Categories.Clear();
+            Products.Clear();
+
+            //заполнение листов
+            foreach (var product in await db.Products.ToListAsync()) 
+            {
+                Products.Add(product);
+            }
+            foreach (var categorya in await db.Categories.ToListAsync())
+            {
+                Categories.Add(categorya);
+            }
+        }
+
+        
+
     }
 }
